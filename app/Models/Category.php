cf472cfe22bb656401c2e67573343a16aa929f14
m0stafa7med <mostafa.ahmed1093@yahoo.com>
2020-09-11 16:29:42 +0200

    committed from VS code

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    //test
     protected $table = 'categories';
     protected $fillable=[
         'name_ar','name_en','created_at','updated_at','active',
